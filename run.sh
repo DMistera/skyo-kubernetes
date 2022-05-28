3ca6daad34fb0f586a274dca95a0166595f2e1e7
dmistera2 <dmistera2@gmail.com>
2022-05-28 20:14:41 +0200

    backend & database

--- run.sh
+++ run.sh
@@ -5,8 +5,8 @@ kubectl create -f ./postgres-storage.yaml
 kubectl create -f ./postgres-deployment.yaml
 kubectl create -f ./postgres-service.yaml
 
-kind load docker-image docker-backend
-kind load docker-image docker-frontend
+kind load docker-image docker_backend
+kind load docker-image docker_frontend
 
 kubectl create -f backend-pod.yaml
 kubectl create -f frontend-pod.yaml
